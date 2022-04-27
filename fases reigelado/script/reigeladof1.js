3d66b1bfd00e2a3735035ba39c810d51f260f3e8
Roberta <roberttavalentim07@icloud.com>
2022-04-26 22:10:47 -0300

    codando reigelado

--- fases reigelado/script/reigeladof1.js
+++ fases reigelado/script/reigeladof1.js
@@ -1,15 +1,15 @@
 function primeiraFaseReiGelado () {
     while (opcao != 1 || opcao != 2) {
         var opcao = prompt (
-            `Escolha uma das opções:\n1. Vira boia e vai embora sozinho.\n2. Decide ajudar todos.`);
+            `Escolha uma das opções:\n1. Decide ir investigar o incêndio.\n2. Decide ir para casa.`);
 
 
         if (opcao == 1) {
-            window.location.href = "../fases reigelado/p3_reigeladogameover1.html";
+            window.location.href = "../fases reigelado/p3_reigelado2.html";
             break;
 
         } else if (opcao == 2) {
-            window.location.href = "../fases reigelado/p3_reigelado2.html";
+            window.location.href = "../fases reigelado/p3_reigeladogameover1.html";
             break;
 
         } else {
@@ -17,3 +17,4 @@ function primeiraFaseReiGelado () {
         }
     }
 }
+
