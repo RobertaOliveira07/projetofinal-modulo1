193c0a70afe7c6132a739ad8265abed474dcc283
Roberta <roberttavalentim07@icloud.com>
2022-04-27 07:03:29 -0300

    finalizado história reigelado

--- fases reigelado/script/reigeladof2.js
+++ fases reigelado/script/reigeladof2.js
@@ -1,15 +1,15 @@
 function segundaFaseReiGelado ( ) {
     while (opcao != 1 || opcao != 2) {
         var opcao = prompt (
-            `Escolha uma das opções:\n1. Enrola a princesa dentro de si para que ela não perca oxigenio.\n2. Vira um cano.`);
+            `Escolha uma das opções:\n1. Tenta abrir a porta para sair.\n2. Sobe para o segundo andar para procurar um extintor.`);
 
 
         if (opcao == 1) {
-            window.location.href = "../fases reigelado/p3_reigelado3.html";
+            window.location.href = "../fases gelado/p3_reigeladogameover2.html";
             break;
 
         } else if (opcao == 2) {
-            window.location.href = "../fases gelado/p3_reigeladogameover2.html";
+            window.location.href = "../fases reigelado/p3_reigelado3.html";
             break;
 
         } else {
--- fases reigelado/script/reigeladof3.js
+++ fases reigelado/script/reigeladof3.js
@@ -1,7 +1,7 @@
 function terceiraFaseReiGelado () {
     while (opcao != 1 || opcao != 2) {
         var opcao = prompt (
-            `Escolha uma das opções:\n1. Ana entende a situação, compra o tabaco e entrega a Matinta.\n2. Ana não acredita na história e segue despreocupada por que só tem mais 1 dia de hospedagem no local.`);
+            `Escolha uma das opções:\n1.  Procura o banheiro para fazer chover sobre o incêndio.\n2. Desiste e vai embora.`);
 
 
         if (opcao == 1) {
